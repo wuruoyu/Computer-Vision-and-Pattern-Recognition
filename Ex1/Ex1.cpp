502cb02af453f8fb4ba8f4683e24e9d6049e67c5
wuruoyu <wuruoyu@me.com>
2017-03-05 04:50:40 -0800

    done

--- Ex1/Ex1.cpp
+++ Ex1/Ex1.cpp
@@ -3,38 +3,42 @@
 #include "CImg.h"
 using namespace cimg_library;
 
-// define color
+// color interface
 class colorSet {
     public:
-        static std::array<int, 3> getRed() {
+        static std::array<unsigned char, 3> getRed() {
             return {255, 0, 0};
         }
 
-        static std::array<int, 3> getGreen() {
+        static std::array<unsigned char, 3> getGreen() {
             return {0, 255, 0};
         }
 
-        static std::array<int, 3> getBlue() {
+        static std::array<unsigned char, 3> getBlue() {
             return {0, 0, 255};
         }
 
-        static std::array<int, 3> getWhite() {
+        static std::array<unsigned char, 3> getWhite() {
             return {255, 255, 255};
         }
 
-        static std::array<int, 3> getBlack() {
+        static std::array<unsigned char, 3> getBlack() {
             return {0, 0, 0};
         }
 
+        static std::array<unsigned char, 3> getYellow() {
+            return {255, 255, 0};
+        }
+
         static bool isWhite(CImg<unsigned char>& image, int x, int y) {
-            std::array<int, 3> white = colorSet::getWhite();
+            std::array<unsigned char, 3> white = colorSet::getWhite();
             if (image(x, y, 0) == white[0] && image(x, y, 1) == white[1] && image(x, y, 2) == white[2])
                 return true;
             return false;
         }
 
         static bool isBlack(CImg<unsigned char>& image, int x, int y) {
-            std::array<int, 3> black = colorSet::getBlack();
+            std::array<unsigned char, 3> black = colorSet::getBlack();
             if (image(x, y, 0) == black[0] && image(x, y, 1) == black[1] && image(x, y, 2) == black[2])
                 return true;
             return false;
@@ -49,15 +53,15 @@ class answer {
         }
         
         static void questionTwo(CImg<unsigned char>& image) {
-           cimg_forXY(image, x, y) {
+            cimg_forXY(image, x, y) {
                if (colorSet::isWhite(image, x, y)) {
-                   std::array<int, 3> red = colorSet::getRed();
+                   std::array<unsigned char, 3> red = colorSet::getRed();
                    image(x, y, 0) = red[0];
                    image(x, y, 1) = red[1];
                    image(x, y, 2) = red[2];
                }
                if (colorSet::isBlack(image, x, y)) {
-                   std::array<int, 3> green = colorSet::getGreen();
+                   std::array<unsigned char, 3> green = colorSet::getGreen();
                    image(x, y, 0) = green[0];
                    image(x, y, 1) = green[1];
                    image(x, y, 2) = green[2];
@@ -65,12 +69,27 @@ class answer {
            };
            image.display();
         }
+
+        static void questionThree(CImg<unsigned char>& image) {
+            std::array<unsigned char, 3> blue = colorSet::getBlue();
+            unsigned char blueChar[3] = {blue[0], blue[1], blue[2]};
+            image.draw_circle(50, 50, 30, blueChar);
+            image.display();
+        }
+
+        static void questionFour(CImg<unsigned char>& image) {
+            std::array<unsigned char, 3> yellow = colorSet::getYellow();
+            unsigned char yellowChar[3] = {yellow[0], yellow[1], yellow[2]};
+            image.draw_circle(50, 50, 3, yellowChar);
+            image.display();
+        }
 };
 
 int main(int argc, char* argv[]) {
     CImg<unsigned char> image("1.bmp");
     
-    //answer::questionOne(image);
-    //answer::questionTwo(image);
+    answer::questionOne(image);
+    answer::questionTwo(image);
     answer::questionThree(image);
+    answer::questionFour(image);
 }
